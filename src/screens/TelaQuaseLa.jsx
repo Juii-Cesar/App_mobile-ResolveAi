57d4ec55ca149525ca46cb14a7b5aff9222b52c0
Júlio César <juliocezinha270@gmail.com>
2026-05-23 17:01:10 -0300

    adicionando alinhamento tela quaseLa

--- src/screens/TelaQuaseLa.jsx
+++ src/screens/TelaQuaseLa.jsx
@@ -12,26 +12,28 @@ export const TelaQuaseLa = () => {
     <SafeAreaView style={styles.container} edges={["bottom", "left", "right"]}>
       <View style={styles.header}>
         <SafeAreaView edges={["top"]} />
-        <Text style={styles.logoContainer}>Quase lá</Text>
+        <Text style={styles.quaseLaContainer}>Quase lá</Text>
       </View>
 
       <View style={styles.content}>
         <Text style={styles.title}>
           Tire uma foto do seu{"\n"}documento de identidade{"\n"}(RG ou CNH)
         </Text>
-        <Text>O que fazer:</Text>
-        <Text>
-          1: Retire o documento do plástico e{"\n"}posicione-o em uma superfície
-          plana e{"\n"}bem iluminada.{"\n"}
-          {"\n"}2: Certifique-se de que todos os dados,{"\n"}especialmente o CPF
-          e a foto, estejam{"\n"}nítidos e sem reflexos de luz.{"\n"}
-          {"\n"}3: Não envie fotos de fotocópias,{"\n"}documentos escaneados ou
-          fotos de{"\n"}outras telas.{"\n"}
-          {"\n"}4: Não corte as bordas do documento e{"\n"}evite cobrir as
-          informações com os dedos.
-        </Text>
-        <Image source={require("../assets/img/exIdentidade.png")} />
-        <Text>
+        <Text style = {styles.subTitle}>O que fazer:</Text>
+        <View style = { styles.infos}>
+          <Text style={styles.instructions}>
+            1: Retire o documento do plástico e{"\n"}posicione-o em uma superfície
+            plana e{"\n"}bem iluminada.{"\n"}
+            {"\n"}2: Certifique-se de que todos os dados,{"\n"}especialmente o CPF
+            e a foto, estejam{"\n"}nítidos e sem reflexos de luz.{"\n"}
+            {"\n"}3: Não envie fotos de fotocópias,{"\n"}documentos escaneados ou
+            fotos de{"\n"}outras telas.{"\n"}
+            {"\n"}4: Não corte as bordas do documento e{"\n"}evite cobrir as
+            informações com os dedos.
+          </Text>
+          <Image source={require("../assets/img/exIdentidade.png")} />
+        </View>
+        <Text style={styles.securityWarning}>
           Seus dados são totalmente confidenciais. Eles serão{"\n"}utilizados
           exclusivamente para a verificação de segurança{"\n"}da plataforma e
           não serão expostos no seu perfil público.
@@ -57,12 +59,12 @@ const styles = StyleSheet.create({
     backgroundColor: BLUE_COLOR,
     height: 220,
     alignItems: "center",
-},
-logoContainer: {
+  },
+  quaseLaContainer: {
     marginTop: 40,
     fontSize: 50,
     fontFamily: "Homenaje_400Regular",
-    color:'#fff'
+    color: "#fff",
   },
   content: {
     flex: 1,
@@ -71,6 +73,7 @@ logoContainer: {
     borderTopLeftRadius: 100,
     alignItems: "center",
     paddingTop: 24,
+    gap: 12,
   },
   title: {
     fontFamily: "Homenaje_400Regular",
@@ -78,20 +81,28 @@ logoContainer: {
     color: BLUE_COLOR,
     textAlign: "left",
   },
-  inputGroup: {
-    width: "100%",
-    gap: 15,
-    marginBottom: 40,
-  },
-  input: {
-    backgroundColor: "#EAEAEA",
-    height: 55,
-    borderRadius: 10,
-    paddingHorizontal: 20,
-    fontSize: 18,
+  subTitle: {
+    color: "#404040",
     fontFamily: "Homenaje_400Regular",
-    color: "#000",
+    fontSize: 16,
+    alignSelf: "flex-start",
+    marginLeft: "5%",
   },
+  infos:{
+    gap:8,
+    alignItems:'center'
+  },
+  instructions: {
+    color: "#404040",
+    fontFamily: "Homenaje_400Regular",
+    fontSize: 16,
+  },
+  securityWarning: {
+    color: "#404040",
+    fontFamily: "Homenaje_400Regular",
+    fontSize:12,
+  },
+
   buttonText: {
     color: "#FFF",
     fontSize: 36,
