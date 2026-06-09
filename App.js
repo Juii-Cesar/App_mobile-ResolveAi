a0539c02c56990af912e564b85cfa896fcd82fef
adriel <adriellfs02@hotmail.com>
2026-06-09 11:12:43 -0300

    fix app.js

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { useFonts, Homenaje_400Regular } from '@expo-google-fonts/homenaje';
-import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { UserTypeProvider } from "./src/context/UserTypeContext";
 import { AuthStack } from './src/navigation/AuthStack';
 import { RegistrationProvider } from './src/context/RegistrationContext';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Homenaje_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <RegistrationProvider> 
       <SafeAreaProvider>
